29f816d188f4d7c051216ffb314b98631f17df81
Tunchanog Powers <tunchanog@gmail.com>
2017-05-04 22:36:56 -0700

    init commit

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -8,7 +8,7 @@ export class Content extends React.Component<ContentProps, undefined> {
     render() {
         return ( 
             <div>
-                 <div className="header"><h1>Alaska Survey App is coming!</h1></div>
+                 <div className="header"><h1>Alaska Survey App is coming soon!</h1></div>
             </div>
         );
     }
